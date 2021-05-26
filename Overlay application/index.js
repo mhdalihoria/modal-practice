2bd536482acf3c2d35f3203ad503e918b5bf81ff
Mhd Ali Houria <alihoria123456@gmail.com>
2021-05-26 13:42:58 +0300

    Add files via upload

---
+++ Overlay application/index.js
@@ -0,0 +1,12 @@
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+})
+
+
+document.getElementById("close-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
+
+document.getElementById("xButton").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
